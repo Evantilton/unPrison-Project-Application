07dcf24e2fdb522d58c0756a434849c9b75e3a27
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-12-14 21:31:45 -0600

    add buttons, inputs, dropdown menu

--- src/components/VenueList/VenueList.js
+++ src/components/VenueList/VenueList.js
@@ -3,7 +3,33 @@ import React, { Component } from 'react';
 class VenueList extends Component {
   render() {
     return (
-      <h1>Venue List component</h1>
+      <div className="venueContainer">
+        <h1>Venue List</h1>
+        <div className="listOptions">
+          <button>Add Venue</button>
+          <input placeholder="Venue Name" />
+          <input placeholder="Contact" />
+          <select>
+            {/* Placeholder values. Waiting for Deborah to reply with types. */}
+
+            <option value="prison">Prison</option>
+            <option value="conference">Conference</option>
+            <option value="school">School</option>
+            <option value="other">Other</option>
+          </select>
+          <button>Create Venue</button>
+          <select>
+            {/* Use a sort table function? */}
+            <option>Order By</option>
+            <option value="prison">Prison</option>
+            <option value="conference">Conference</option>
+            <option value="school">School</option>
+            <option value="other">Other</option>
+          </select>
+          <button>Create Venue</button>
+        </div>
+
+      </div>
     )
   }
 }
