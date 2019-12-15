338da07b91a04e752f6ac8adb1822aa16cc11d08
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-12-15 11:33:54 -0600

    comments, formatting

--- src/components/VenueList/VenueList.js
+++ src/components/VenueList/VenueList.js
@@ -6,9 +6,13 @@ class VenueList extends Component {
       <div className="venueContainer">
         <h1>Venue List</h1>
         <div className="listOptions">
+
           <button>Add Venue</button>
+
           <input placeholder="Venue Name" />
+
           <input placeholder="Contact" />
+
           <select>
             {/* Placeholder values. Waiting for Deborah to reply with types. */}
 
@@ -26,29 +30,30 @@ class VenueList extends Component {
             <option value="school">School</option>
             <option value="other">Other</option>
           </select>
+
           <button>Create Venue</button>
         </div>
 
         <div className="listVenues"></div>
-
-        <table>
- <tbody>
-  <tr>
-    <td>St. Cloud Pen
+        {/* Using table without headers for potential sort system and thinking we can style it to look like what we want. */}
+        <table border="1">
+          <tbody>
+            <tr>
+              <td>St. Cloud Pen
       <br />Primary Contact: Sarah Smith
-    </td>
-    <td>Phone: 123-456-7890</td>
-    <td>Address: 1000 maple st.</td>
-  </tr>
-  <tr>
-  <td>Ramsey County Prison
+              </td>
+              <td>Phone: 123-456-7890</td>
+              <td>Address: 1000 maple st.</td>
+            </tr>
+            <tr>
+              <td>Ramsey County Prison
       <br />Primary Contact: Benjamin Brown
-    </td>
-    <td>Phone: 123-456-7890</td>
-    <td>Address: 4300 maple st.</td>
-  </tr>
-  </tbody>
-</table>
+              </td>
+              <td>Phone: 321-456-7890</td>
+              <td>Address: 4300 maple st.</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
