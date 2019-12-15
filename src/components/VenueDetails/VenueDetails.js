bd6d00e60fc621671778cff0750ae07d8ae3ccd8
Chris Ferber <chris@ferbers.us>
2019-12-15 04:00:20 -0600

    conditionally render General component or Events component on click of respective tab

--- src/components/VenueDetails/VenueDetails.js
+++ src/components/VenueDetails/VenueDetails.js
@@ -4,28 +4,51 @@ import VenueEvents from './Events/Events';
 import './VenueDetails.css';
 
 class VenueDetails extends Component {
+
+    state = {
+        general: true,
+        events: false,
+    }
+
+    handleGeneralTabClick = () => {
+        this.setState({
+            general: true,
+            events: false,
+        });
+    }
+
+    handleEventsTabClick = () => {
+        this.setState({
+            general: false,
+            events: true,
+        });
+    }
+
     render() {
         return (
             <>
-            <div className="header">
+                <div className="header">
                     <h1>Venue Details Component</h1>
                 </div>
-            <div className="container">
                 <div className="mainWindow">
                     <div className="mainInfo">
                         <p>Area for venue name + address and primary contact info</p>
                     </div>
-                    <div className="generalTab">
+                    <div className="generalTab" onClick={this.handleGeneralTabClick}>
                         General
                     </div>
-                    <div className="eventsTab">
+                    <div className="eventsTab" onClick={this.handleEventsTabClick}>
                         Events
                     </div>
-                    <div className="tabWindow">
-                        <VenueEvents />
-                    </div>
+                    {this.state.events ?
+                        <div className="tabWindow">
+                            <VenueEvents />
+                        </div> :
+                        <div className="tabWindow">
+                            <VenueGeneral />
+                        </div>
+                    }
                 </div>
-            </div>
             </>
         )
     }
