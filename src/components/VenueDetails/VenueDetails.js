1a6ffb616e771b98effc25dbd807001a39dafee1
Chris Ferber <chris@ferbers.us>
2019-12-15 03:44:40 -0600

    create basic css grid layout to match wireframe

--- src/components/VenueDetails/VenueDetails.js
+++ src/components/VenueDetails/VenueDetails.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import VenueGeneral from './General/General';
 import VenueEvents from './Events/Events';
+import './VenueDetails.css';
 
 class VenueDetails extends Component {
     render() {
         return (
             <>
-                <div>
+            <div className="header">
                     <h1>Venue Details Component</h1>
                 </div>
-                <div>
-                    <VenueGeneral />
+            <div className="container">
+                <div className="mainWindow">
+                    <div className="mainInfo">
+                        <p>Area for venue name + address and primary contact info</p>
+                    </div>
+                    <div className="generalTab">
+                        General
+                    </div>
+                    <div className="eventsTab">
+                        Events
+                    </div>
+                    <div className="tabWindow">
+                        <VenueEvents />
+                    </div>
                 </div>
-                <div>
-                    <VenueEvents />
-                </div>
+            </div>
             </>
         )
     }
