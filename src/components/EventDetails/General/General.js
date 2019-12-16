2e942a3edb0746638197e7f9a0bd5169e8de451b
Luke Schleder <luke.schleder@gmail.com>
2019-12-16 12:56:20 -0600

    create input fields for general tab of event details

--- src/components/EventDetails/General/General.js
+++ src/components/EventDetails/General/General.js
@@ -5,6 +5,73 @@ class General extends Component {
         return (
             <div>
                 <h3>General Component</h3>
+                    <ul class="nobull">
+                        <li >
+                            <label>Date Last Contacted:</label>
+                            <input type="date"></input>
+                        </li>
+                        <li>
+                            <label>Best Day of Week:</label>
+                            <input type="text"></input>
+                        </li>
+                        <li>
+                            <label>Best Times:</label>
+                            <input type="text"></input>
+                        </li>
+                        <li>
+                            <label>Proposed Month:</label>
+                            <input type="text"></input>
+                        </li>
+                        <li>
+                            <label>Proposed Dates:</label>
+                            <input type="date"></input>
+                            <input type="date"></input>
+                        </li>
+                        <li>
+                            <label>Confirmed Date:</label>
+                            <input type="date"></input>
+                        </li>
+                        <li>
+                            <label>Desired Focus:</label>
+                            <input type="text"></input>
+                        </li>
+                        <li>
+                            <label>Total Prison Count:</label>
+                            <input type="number"></input>
+                        </li>
+                        <li>
+                            <label>Expected Attendance:</label>
+                            <input type="number"></input>
+                        </li>
+                        <li>
+                            <label>Room Location:</label>
+                            <input type="text"></input>
+                        </li>
+                        <li>
+                            <label>Actual Attendance:</label>
+                            <input type="number"></input>
+                        </li>
+                        <li>
+                            <label>Demographics:</label>
+                            <input type="text"></input>
+                        </li>
+                        <li>
+                            <label>Flyer Mailed:</label>
+                            <input type="checkbox"></input>
+                        </li>
+                        <li>
+                            <label>Flyer Mailed Date:</label>
+                            <input type="date"></input>
+                        </li>
+                        <li>
+                            <label>Heard About:</label>
+                            <input type="text"></input>
+                        </li>
+                        <li>
+                            <label>Event Notes:</label>
+                            <input type="text"></input>
+                        </li>
+                    </ul>
             </div>
         )
     }
