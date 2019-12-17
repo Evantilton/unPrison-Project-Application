955c68c7dfdd8d84f3d2527348b7434b6b68ad61
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-12-17 15:17:29 -0600

    created correct venue saga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,7 +2,7 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './loginSaga';
 import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
-import venueSaga from '.venueSaga';
+import venueSaga from './venueSaga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
