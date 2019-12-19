7ecdacc79ffd7d651e1b3ebdc6ccb68201e7cb39
Chris Ferber <chris@ferbers.us>
2019-12-19 14:44:00 -0600

    get first two input fields in eventDetails Travel tab to update reducer onChange

--- src/redux/reducers/eventsTravelReducer.js
+++ src/redux/reducers/eventsTravelReducer.js
@@ -1,8 +1,13 @@
 // stores all data from event table in database
-const eventsTravelReducer = (state = [], action) => {
+const eventsTravelReducer = (state = {}, action) => {
     switch (action.type) {
         case 'SET_TRAVEL_TABLE':
             return action.payload;
+        case 'SET_EXISTING_TRAVEL':
+           return {
+               ...state,
+               [action.payload.property]: action.payload.value,
+           }
         default:
             return state;
     }
